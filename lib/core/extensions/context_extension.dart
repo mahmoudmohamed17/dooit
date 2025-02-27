fcaa9516e520ff90e3280a8a793e08c03e6952e4
mahmoudmohamed17 <ma5975986@gmail.com>
2025-02-27 17:41:48 +0200

    some edits

--- lib/core/extensions/context_extension.dart
+++ lib/core/extensions/context_extension.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 extension ContextExtension on BuildContext {
-  double get height => MediaQuery.of(this).size.height;
-  double get width => MediaQuery.of(this).size.width;
+  double get height => MediaQuery.sizeOf(this).height;
+  double get width => MediaQuery.sizeOf(this).width;
 
   double get safeHeight =>
       height -
-      MediaQuery.of(this).padding.top -
-      MediaQuery.of(this).padding.bottom;
+      MediaQuery.paddingOf(this).top -
+      MediaQuery.paddingOf(this).bottom;
   double get safeWidth =>
       width -
-      MediaQuery.of(this).padding.left -
-      MediaQuery.of(this).padding.right;
+      MediaQuery.paddingOf(this).right -
+      MediaQuery.paddingOf(this).left;
 }
