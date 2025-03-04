83312dcef406cea5d3597fdd35a436b0d6518c94
mahmoudmohamed17 <ma5975986@gmail.com>
2025-03-04 13:36:22 +0200

    create category model and task model

--- lib/core/models/category_model.dart
+++ lib/core/models/category_model.dart
@@ -1,1 +1,14 @@
-class CategoryModel {}
+class CategoryModel {
+  final int id;
+  final String date;
+  String title;
+  String label;
+  bool isPinned;
+
+  CategoryModel(
+      {required this.id,
+      required this.date,
+      this.title = '',
+      this.label = '',
+      this.isPinned = false});
+}
--- lib/core/models/task_model.dart
+++ lib/core/models/task_model.dart
@@ -1,1 +1,12 @@
-class TaskModel {}
+class TaskModel {
+  final int id;
+  final int categoryId;
+  String title;
+  bool isChecked;
+
+  TaskModel(
+      {required this.id,
+      required this.categoryId,
+      this.title = '',
+      this.isChecked = false});
+}
